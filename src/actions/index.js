ee5e95311eb0db7cfcd83a780bb97abcdffaff87
andela-hms <humphrey.musonye@andela.com>
2017-12-28 16:51:44 +0300

    (feat): Redirect user once logged in

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const API_URL = 'http://127.0.0.1:5000/api/v1.0/';
 
 export function SignInUser({ email, password }) {
     return function(dispatch) {
         // Submit email/password to server
-        axios.post(`${API_URL}auth/login/`, {email, password});
+        axios.post(`${API_URL}auth/login/`, {email, password})
+            .then(response=> {
+                // if request is good ...
+                // - update state to indicate user is auth
+                // - save the jwt token
+                // - redirect to route 'feature'
+                // If request is bad ...
+                // - show an error to the user
+                browserHistory.push('/');
+            })
+            .catch(() => {
 
-        // if request is good ...
-        // - update state to indicate user is auth
-        // - save the jwt token
-        // - redirect to route 'feature'
-
-        // If request is bad ...
-        // - show an error to the user
+            });
     }
 }
